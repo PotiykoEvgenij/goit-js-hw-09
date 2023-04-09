ef9447fb2a82da9a5f1184d2ed100b9a73fae994
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-04-09 18:08:52 +0300

    hw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const body = document.body;
+let intervalId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+}
+
+function startChangeBgColor() {
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+}
+
+function stopChangeBgColor() {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+}
+
+startBtn.addEventListener("click", startChangeBgColor);
+stopBtn.addEventListener("click", stopChangeBgColor);
